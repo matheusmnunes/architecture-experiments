9b1938b6b74bd6a7803f852705ba6495f7660ef2
Matheus Nunes <matheusnunes.ufv@gmail.com>
2026-08-14 01:07:21 -0300

    - versao

--- services/core-api/src/v1/routes/phone-type.route.ts
+++ services/core-api/src/v1/routes/phone-type.route.ts
@@ -1,19 +1,20 @@
+import type { RouterContext } from '@koa/router';
 import {get, insert,update} from '../services/phone-type.service.js';
 
 
 const phone = (router: any) => 
     router
-        .get('/phonetype', async(ctx) => {
+        .get('/phonetype', async(ctx:RouterContext) => {
             ctx.body = await get(ctx.state.input.query ?? {});
         })
-        .get('/phonetype/:id', async(ctx:any) => {
+        .get('/phonetype/:id', async(ctx:RouterContext) => {
             ctx.body = await get(Object.assign(ctx.params,ctx.query));
         })
-        .post('/phonetype', async(ctx:any) => {
+        .post('/phonetype', async(ctx:RouterContext) => {
             const res = await insert(ctx.request.body);
             ctx.body =  {success: res.affectedRows > 0 , id: res};
         })
-        .put('/phonetype/:i18n_id', async(ctx:any) => {
+        .put('/phonetype/:i18n_id', async(ctx:RouterContext) => {
             const res = await update(Object.assign(ctx.params, ctx.request.body));
             ctx.body =  {success: res };
         })
